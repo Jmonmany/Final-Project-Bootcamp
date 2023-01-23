74982f2341ed0add7c3d192fbc258f4e7c2c2234
Javi <monmany1992@gmail.com>
2023-01-23 13:48:21 +0100

    Added address component and firebase app

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,5 +1,5 @@
-import { loginWithGoogle } from "../../../config";
-import { Uploader } from "../uploader/uploader";
+// import { loginWithGoogle } from "../../../config";
+// import { Uploader } from "../uploader/uploader";
 
 export function Header({ children }: { children: JSX.Element }) {
     const title = 'Marina Labella';
@@ -8,9 +8,9 @@ export function Header({ children }: { children: JSX.Element }) {
         <header>
             <h1>{title}</h1>
             <h2>{subtitle}</h2>
-            <button onClick={loginWithGoogle}>Login</button>
+            {/* <button onClick={loginWithGoogle}>Login</button> */}
             {children}
-            <Uploader></Uploader>
+            {/* <Uploader></Uploader> */}
         </header>
     );
 }
